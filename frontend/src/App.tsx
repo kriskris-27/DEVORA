882a63712be8079570b7621beec828388adb3269
Kris <krishnakumars0101@gmail.com>
2025-09-25 23:59:25 +0530

    testing tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -86,8 +86,8 @@ const App: React.FC = () => {
       }}>
         
         
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
+        <h1 className="text-3xl font-bold underline p-4">
+      Hello world! 
     </h1>   
         <h3>Features</h3>
         <ul>
