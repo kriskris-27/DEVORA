626fde1f5d9d555cfed498c0ed7b9eed6c8914b7
Kris <krishnakumars0101@gmail.com>
2025-10-11 12:23:48 +0530

    fixed

--- client/src/Routes/AppRoutes.tsx
+++ client/src/Routes/AppRoutes.tsx
@@ -14,6 +14,7 @@ import Partners from "../pages/Partners";
 import Grievance from "../pages/Grievance";
 import AppLayout from "../Layouts/AppLayout";
 import { ProtectedMechanicRoute } from "../App";
+import MechanicDashboard from "../pages/MechanicDashboard";
 
 const AppRoutes = () => {
   return (
@@ -39,6 +40,7 @@ const AppRoutes = () => {
           </ProtectedMechanicRoute>
         }>
           <Route path="onboarding" element={<MechanicOnboarding />} />
+          <Route path="dashboard" element={<MechanicDashboard />}/>
         </Route>
 
         {/* Default redirect */}
